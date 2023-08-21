0c57397cc2303375e8ee565b0d82650ffcb5b3bc
Adrian <adrianqueizan@gmail.com>
2023-08-21 12:04:23 -0400

    contrlos flow lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  if (value <= 400){
+    return "This one is on me!"
+  }else if (value > 400 && value <= 2000){
+    return "That will be twenty bucks."
+  }else  if(value > 2000 && value < 2500){
+    return "I will gladly take your thirty bucks."
+  }else if(value >= 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(value){
   // Write your code here!
+return (value === "NYC")? "Ok, sounds good.":"No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(value){
   // Write your code here!
+  switch (value){
+    case "generous":
+    return  "Thank you so much."
+
+    case "not as generous":
+      return "Thank you.";
+      default :
+      return "Bye."
+    
+  }
 }
